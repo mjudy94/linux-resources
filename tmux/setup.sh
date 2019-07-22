136fa678139b806d3492b0503371ec4e45f219b0
Matt Judy <mjudy@carekinesis.com>
2019-07-22 21:05:14 +0000

    Fixed typo in tmux setup script

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -2,7 +2,7 @@
 
 script_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-if [[ ! -a ~/tmux_conf.sym]]
+if [[ ! -a ~/tmux_conf.sym ]]
 then
   ln -s $script_dir/tmux_conf.sym ~/.tmux.conf
 fi
